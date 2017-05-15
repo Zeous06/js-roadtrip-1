b1db9b62858fe966a89724e4c83664a2bacb03bd
Zeous06 <edwinhernandezaponte@yahoo.com>
2017-05-15 19:41:41 -0400

    first commit

---
+++ 1.5_A_Boolean_Expression.js
@@ -0,0 +1,6 @@
+// Video 1.1 accompanies this exercise
+
+// In the console, write a Boolean expression that tests
+// whether two numbers are equal, and whose answer evaluates to false.
+3 === "3"
+console.log( 4 === 3); //  boolean false
